71b097e661b509d31a5689bf328c5585fffd0a51
Dale Zak <dalezak@gmail.com>
2017-07-06 09:14:20 -0600

    Updated translations for #123

--- hooks/after_clean/010_translate_phrases.js
+++ hooks/after_clean/010_translate_phrases.js
@@ -1,84 +1,83 @@
 #!/usr/bin/env node
 
-module.exports = function(context) {
-  const translate = require('google-translate-api');
-  var languages = ['ar', 'fr', 'es', 'de', 'pt', 'ru', 'nl', 'sw'];
-  var fs = context.requireCordovaModule('fs');
-  var root = context.opts.projectRoot;
-  var assets = root + "/src/assets/i18n/";
-  var input = assets + "en.json";
-  var english = require(input);
-  console.log("File Source " + input);
-  var openFile = function(path) {
-    return new Promise((resolve, reject) => {
-      fs.open(path, 'rs+', (err, fd) => {
-        console.log("File Opened " + path);
-        resolve(fd);
-      });
+const translate = require('google-translate-api');
+var languages = ['ar', 'fr', 'es', 'de', 'pt', 'ru', 'nl', 'sw'];
+var fs = require('fs');
+var root = process.argv[2];
+var assets = root + "/src/assets/i18n/";
+var input = assets + "en.json";
+var english = require(input);
+
+console.log("File Source " + input);
+var openFile = function(path) {
+  return new Promise((resolve, reject) => {
+    fs.open(path, 'rs+', (err, fd) => {
+      console.log("File Opened " + path);
+      resolve(fd);
     });
-  };
-  var startFile = function(path) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(path, "{", 'utf8', (res) => {
-        console.log("File Started " + path);
-        resolve();
-      });
+  });
+};
+var startFile = function(path) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, "{", 'utf8', (res) => {
+      console.log("File Started " + path);
+      resolve();
     });
-  };
-  var closeFile = function(path, fd) {
-    return new Promise((resolve, reject) => {
-      fs.appendFile(path, "\n}", 'utf8', (res) => {
-        if (fd) {
-          fs.close(fd, () => {
-            console.log("File Closed " + path);
-            resolve();
-          });
-        }
-        else {
+  });
+};
+var closeFile = function(path, fd) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(path, "\n}", 'utf8', (res) => {
+      if (fd) {
+        fs.close(fd, () => {
+          console.log("File Closed " + path);
           resolve();
-        }
-      });
-    });
-  };
-  var writeFile = function(path, line) {
-    return new Promise((resolve, reject) => {
-      fs.appendFile(path, line, 'utf8', (res) => {
-        resolve(true);
-      });
-    });
-  };
-  var googleTranslate = function(path, language, key, value, comma) {
-    return new Promise((resolve, reject) => {
-      translate(value, {to:language}).then(
-        (translation) => {
-          let newline = comma ? ",\n" : "\n";
-          let line = newline + JSON.stringify(key) + ": " + JSON.stringify(translation.text);
-          console.log(line);
-          resolve(line);
-        },
-        (error) => {
-          console.error(error);
-          reject(error);
         });
+      }
+      else {
+        resolve();
+      }
     });
-  };
-  for (let language of languages) {
-    let path = assets + language + ".json";
-    openFile(path).then((fd) => {
-      startFile(path).then(() => {
-        let promise = Promise.resolve(false);
-        for (let key in english) {
-          let value = english[key];
-          promise = promise.then((comma) => {
-            return googleTranslate(path, language, key, value, comma);
-          }).then((line) => {
-            return writeFile(path, line);
-          });
-        }
-        promise.then((done)=> {
-          return closeFile(path, fd);
+  });
+};
+var writeFile = function(path, line) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(path, line, 'utf8', (res) => {
+      resolve(true);
+    });
+  });
+};
+var googleTranslate = function(path, language, key, value, comma) {
+  return new Promise((resolve, reject) => {
+    translate(value, {to:language}).then(
+      (translation) => {
+        let newline = comma ? ",\n" : "\n";
+        let line = newline + JSON.stringify(key) + ": " + JSON.stringify(translation.text);
+        console.log(line);
+        resolve(line);
+      },
+      (error) => {
+        console.error(error);
+        reject(error);
+      });
+  });
+};
+for (let language of languages) {
+  let path = assets + language + ".json";
+  openFile(path).then((fd) => {
+    startFile(path).then(() => {
+      let promise = Promise.resolve(false);
+      for (let key in english) {
+        let value = english[key];
+        promise = promise.then((comma) => {
+          return googleTranslate(path, language, key, value, comma);
+        }).then((line) => {
+          return writeFile(path, line);
         });
+      }
+      promise.then((done)=> {
+        return closeFile(path, fd);
       });
     });
-  }
+  });
 }
