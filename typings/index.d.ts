8a6da0cd5571447cd04f5452537c426f8a235504
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-10 21:03:46 +0800

    Delete typings directory

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/leaflet/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/reflect-metadata/index.d.ts" />
